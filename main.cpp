7071f0f9ada1befaf11d32c97832998aada3c914
Ilya Shlom <ilyashlom32@gmail.com>
2022-12-12 23:30:22 +0300

    v3: working with all permutations of power n

--- main.cpp
+++ main.cpp
@@ -1,24 +1,72 @@
 #include <iostream>
+#include <cmath>
 #include "tableaux.h"
 
 using namespace std;
 
-int main() {
-    int *mas;
-    mas = new int[5];
-    mas[0] = 5;
-    mas[1] = 2;
-    mas[2] = 3;
-    mas[3] = 1;
-    mas[4] = 4;
-    Permutation A(5, mas);
-    cout << "Done\n";
-    Tableaux T(A.reverse().reverse());
+int *ithPermutation(const int n, int i); // генерация n-ой перестановки
 
-    A.Show();
-    A.complement().Show();
-    T.Show();
+int main() {
+    int n = 5; // Мощность перестановок
+
+    // Создание всех перестановок мощности n
+    int pAmount = static_cast<int>(tgamma(n+1));
+    int **mas;
+    mas = new int*[pAmount];
+    for (int i = 0; i < pAmount; i++) {
+        mas[i] = new int[n];
+        memcpy(mas[i], ithPermutation(n, i), sizeof(int)*n);
+    }
+
+    Permutation A(n);
+    Tableaux T(A);
+
+//    cout << "Random permutation: \n";
+//    A.Show();
+//    A.reverse().complement();
+//    A.Show();
+//    T.Show();
+
+    cout << "Processing all permutations of power " << n << endl;
+    for (int i = 0; i < pAmount; i++) {
+        A.Rebuild(mas[i]);
+        A.Show();
+        A.reverse().complement();
+        T.Rebuild(A);
+        A.Show();
+        T.Show();
+    }
+
 
 
     return 0;
+}
+
+int *ithPermutation(const int n, int i)
+{
+    int j, k = 0;
+    int *fact = (int *)calloc(n, sizeof(int));
+    int *perm = (int *)calloc(n, sizeof(int));
+
+    // compute factorial numbers
+    fact[k] = 1;
+    while (++k < n)
+        fact[k] = fact[k - 1] * k;
+
+    // compute factorial code
+    for (k = 0; k < n; ++k)
+    {
+        perm[k] = i / fact[n - 1 - k] + 1;
+        i = i % fact[n - 1 - k];
+    }
+
+    // readjust values to obtain the permutation
+    // start from the end and check if preceding values are lower
+    for (k = n - 1; k > 0; --k)
+        for (j = k - 1; j >= 0; --j)
+            if (perm[j] <= perm[k])
+                perm[k]++;
+    free(fact);
+
+    return perm;
 }
